38b92423ccab502128d1a5a492dbefd691f494b1
whillourd <torreno.mark1999@gmail.com>
2022-09-08 01:42:35 +0800

    update

--- members/views.py
+++ members/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
@@ -15,7 +16,7 @@ def homepage(request):
 def login_page(request):
     
     if request.method == 'POST':
-        user = auth.authenticate(username=request.POST['username'],password = request.POST['password'])
+        user = auth.authenticate(username=request.POST['username']  ,password = request.POST['password'])
         if user is not None:
             auth.login(request,user)
             return redirect('information')
